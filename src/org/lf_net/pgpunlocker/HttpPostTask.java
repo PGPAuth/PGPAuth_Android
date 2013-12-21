61d03db95d3e633a9fe73014006b2268628caf73
Moritz Grosch (LittleFox) <littlefox@fsfe.org>
2013-12-22 00:06:43 +0100

    Fixed Bug #1
    Added POST-Parameter to request and increased version numbers

--- src/org/lf_net/pgpunlocker/HttpPostTask.java
+++ src/org/lf_net/pgpunlocker/HttpPostTask.java
@@ -11,6 +11,7 @@ import org.apache.http.client.*;
 import org.apache.http.client.methods.*;
 import org.apache.http.impl.client.*;
 import org.apache.http.message.*;
+import org.apache.http.client.entity.*;
 
 public class HttpPostTask extends AsyncTask<String,Long,String>
 {
@@ -22,6 +23,7 @@ public class HttpPostTask extends AsyncTask<String,Long,String>
 			HttpPost req = new HttpPost(texts[0]);
 			List<NameValuePair> params = new ArrayList<NameValuePair>();
 			params.add(new BasicNameValuePair("data", texts[1]));
+			req.setEntity(new UrlEncodedFormEntity(params));
 
 			HttpResponse response = client.execute(req);
 
