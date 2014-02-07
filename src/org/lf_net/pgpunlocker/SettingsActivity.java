14d8bc416b87d1822421f6b88f9d956889c55a00
Moritz Grosch (LittleFox) <littlefox@fsfe.org>
2014-02-07 22:31:13 +0100

    Removed warnings²

--- src/org/lf_net/pgpunlocker/SettingsActivity.java
+++ src/org/lf_net/pgpunlocker/SettingsActivity.java
@@ -1,16 +1,12 @@
 
 package org.lf_net.pgpunlocker;
 
-import android.app.*;
-import android.os.*;
-import android.view.*;
-import android.widget.*;
-import android.util.*;
-import android.content.*;
-import android.preference.*;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
 public class SettingsActivity extends PreferenceActivity {
-    @Override
+    @SuppressWarnings("deprecation")
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
